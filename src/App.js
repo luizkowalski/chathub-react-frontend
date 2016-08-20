d47c5b3c440d9eb8095c2fc8628f8d63fde4516b
Luiz Eduardo <luizeduardokowalski@gmail.com>
2016-08-21 01:11:48 +0200

    Login with github

--- src/App.js
+++ src/App.js
@@ -8,11 +8,40 @@ injectTapEventPlugin();
 
 const App = React.createClass({
   getInitialState(){
-    return { loggedIn: false };
+    return { loggedIn: localStorage.getItem('loggedIn') || false };
+  },
+
+  logged(){
+    return localStorage.getItem('loggedIn' || false);
+  },
+
+  doLogout(){
+    localStorage.setItem('loggedIn', false);
+    this.setState({loggedIn: false});
   },
 
   doLogin(){
-    this.setState({loggedIn: !this.state.loggedIn});
+    var component = this;
+    var provider = new firebase.auth.GithubAuthProvider();
+    provider.addScope('user:email');
+    provider.addScope('repo');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(token);
+      console.log(user);
+      localStorage.setItem('loggedIn', true);
+      this.setState({loggedIn: true});
+      // ...
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+      console.log(errorCode+ " - "+errorMessage)
+    });
   },
 
   render() {
@@ -20,18 +49,17 @@ const App = React.createClass({
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Chathub</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, login with your Github account
         </p>
-        <RaisedButton onClick={this.doLogin} label="Sign-in"/>
 
-          {this.state.loggedIn ? (
-            <Link to="/logout">Log out</Link>
-          ) : (
-            <Link to="/login">Sign in</Link>
-          )}
+        { this.logged ? (
+          <RaisedButton onClick={this.doLogout} label="Logout"/>
+        ) : (
+          <RaisedButton onClick={this.doLogin} label="Sign-in"/>
+        )}
       </div>
     );
   }
